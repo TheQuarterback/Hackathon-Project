273a2e84ef78600516a51102371c60f2aca1730a
unknown <weiherngmy@hotmial.com>
2022-01-16 16:19:34 +0800

    updated the constants file

--- constants.py
+++ constants.py
@@ -15,9 +15,26 @@
 ### These are constants for all cards and the decks for the game. ###
 ### Do not delete ###
 
+from lib2to3.pgen2.token import GREATER
+
+
 ALL_COLOURS = "BGYPORM"
 ALL_NUM = "0123456789"
 PLUS_2 ="+2"
 PLUS_4 ="+4"
 WILDCARDS = [PLUS_2, PLUS_4]
 
+# review 
+
+BLUE_SUIT = ["B0","B1","B2","B3","B4","B5","B6","B7","B8","B9"]
+RED_SUIT = ["R0","R1","R2","R3","R4","R5","R6","R7","R8","R9"]
+YELLOW_SUIT = ["Y0","Y1","Y2","Y3","Y4","Y5","Y6","Y7","Y8","Y9"]
+GREEN_SUIT = ["G0","G1","G2","G3","G4","G5","G6","G7","G8","G9"]
+ORANGE_SUIT = ["O0","O1","O2","O3","O4","O5","O6","O7","O8","O9"]
+PURPLE_SUIT = ["P0","P1","P2","P3","P4","P5","P6","P7","P8","P9"]
+MAGENTA_SUIT = ["M0","M1","M2","M3","M4","M5","M6","M7","M8","M9"]
+
+INITIAL_DECK = [BLUE_SUIT, RED_SUIT,YELLOW_SUIT,GREEN_SUIT,ORANGE_SUIT,
+                PURPLE_SUIT,MAGENTA_SUIT]
+
+PLAY_DECK = INITIAL_DECK + WILDCARDS
